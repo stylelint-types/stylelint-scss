ed48556b58f8cecc0866575167e6ad10d074074c
pengzhanbo <volodymyr@foxmail.com>
2024-08-20 00:45:52 +0800

    feat: add `property-no-unknown` add support for nested declarations

--- lib/rules/property-no-unknown.d.ts
+++ lib/rules/property-no-unknown.d.ts
@@ -4,8 +4,18 @@ export type PropertyNoUnknownOptions = RuleConfig<
   true,
   {
     ignoreProperties?: (string | RegExp)[]
+    /**
+     * Skips checking properties of the given selectors against this rule.
+     */
     ignoreSelectors?: (string | RegExp)[]
+    /**
+     * Ignores properties nested within specified at-rules.
+     */
     ignoreAtRules?: (AtRules | RegExpLike)[]
+    /**
+     * If `true`, this rule will check vendor-prefixed properties.
+     * @default false
+     */
     checkPrefixed?: boolean
   }
 >
