892528754f2747d2a195a594916340f6e6c23d35
pengzhanbo <volodymyr@foxmail.com>
2026-05-04 01:59:06 +0800

    fix: incorrect type for `scss/declaration-nested-properties`, close #1

--- src/rules/declaration-nested-properties.d.ts
+++ src/rules/declaration-nested-properties.d.ts
@@ -1,5 +1,12 @@
-import type { RuleConfig } from 'stylelint-define-config'
+import type { RuleConfig, LiteralUnion } from 'stylelint-define-config'
 
 export type DeclarationNestedPropertiesOptions = RuleConfig<
-  'always' | 'never'
+  'always' | 'never',
+  [prop: string],
+  {
+    /**
+     * Works only with `"always"` and reverses its effect for a property if it is the only one with the corresponding "namespace" (e.g. `margin` in `margin-top`) in a ruleset.
+     */
+    except?: LiteralUnion<'only-of-namespace'>[]
+  }
 >
