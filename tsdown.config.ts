2d6c82f42b0b5cba8ffe8480f7c6e17aa996094a
pengzhanbo <volodymyr@foxmail.com>
2026-05-04 02:10:49 +0800

    build: migrate tsup to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: 'esm',
   outDir: 'lib',
   dts: true,
-  clean: true,
+  fixedExtension: false,
 })
